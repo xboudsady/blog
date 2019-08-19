4fc5d4c6c431f36e0a1ba38dbc4993730a1e384f
xboudsady <boudsadyx@gmail.com>
2019-08-19 23:43:09 +0200

    Created folder for action creators. Define our first action, as type FETCH_POSTS, to call the api data. Created the componentDidMount() method to load the fetchPosts() action creator, which is passed on in the connect() method. 1st argument on connect() is null, as there is no initial state.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
+  
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+  
   render() {
     return <div>Post List</div>
   }
 };
 
-export default PostList;
+
+// 1st argument on connect function as 'null' as we don't have a state currently
+// 2nd argument, action creator
+export default connect(null, { fetchPosts } )(PostList);
