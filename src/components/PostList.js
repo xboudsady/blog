09d621b3051495abc91fe2214803a77a1323837d
xboudsady <boudsadyx@gmail.com>
2019-08-21 21:45:15 +0200

    Updated action creator FETCH_POSTS to return only response.data

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,17 +3,22 @@ import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
-  
   componentDidMount() {
     this.props.fetchPosts();
   }
   
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>
   }
 };
 
+// Anytime we need to access state in redux, we need to call this function, to pass into the connect()
+const mapStateToProps = state => {
+  return { posts: state.posts };
+}
+
 
 // 1st argument on connect function as 'null' as we don't have a state currently
 // 2nd argument, action creator
-export default connect(null, { fetchPosts } )(PostList);
+export default connect(mapStateToProps, { fetchPosts } )(PostList);
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,7 +1,7 @@
 // Set the initital default state being returned as an emptty array
 export default (state = [], action) => {
   switch (action.type) {
-    case 'FETCH_POST':
+    case 'FETCH_POSTS':
       return action.payload;
     default:
       return state;
