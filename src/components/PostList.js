567fb09eac01006a8ec34e55362e3eab9e6a9b67
xboudsady <boudsadyx@gmail.com>
2019-08-21 21:55:36 +0200

    Added reunderList() to render JSX of data recevied.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,13 +3,30 @@ import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
+  // Upon component render, call the fetchPosts() method, from the action creator for API data
   componentDidMount() {
     this.props.fetchPosts();
   }
+
+  // Displaying our data
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={ post.id }>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{ post.title }</h2>
+              <p>{ post.body }</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
   
   render() {
-    console.log(this.props.posts);
-    return <div>Post List</div>
+    return <div className="ui relaxed divided list">{this.renderList()}</div>
   }
 };
 
