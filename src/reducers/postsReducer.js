94882785c84859d21f35c87661f57d6617b33343
xboudsady <boudsadyx@gmail.com>
2019-08-21 21:32:16 +0200

    Updated reducer function, for action type FETCH_POST

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,9 @@
-export default () => {
-  return 123;
+// Set the initital default state being returned as an emptty array
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_POST':
+      return action.payload;
+    default:
+      return state;
+  }
 };
